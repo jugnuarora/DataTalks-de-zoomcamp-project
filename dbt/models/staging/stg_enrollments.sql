a9183ac6cc60a9dc066b663835007900ddaf6f94
Jugnu Arora <jugnu.arora@gmail.com>
2025-03-27 20:26:23 +0000

    commit from dbt after formacode download update

--- dbt/models/staging/stg_enrollments.sql
+++ dbt/models/staging/stg_enrollments.sql
@@ -27,13 +27,12 @@ FROM
     LEFT JOIN {{ref('stg_courses')}} as c
     on (lower(e.provider) = lower(c.provider)
         AND e.provider_id = c.provider_id
-        AND e.certification_title = c.certification_title
+        AND lower(e.certification_title) = lower(c.certification_title)
         AND e.code_certification = c.code_certification
         AND e.code_rncp = c.code_rncp
         AND e.code_rs = c.code_rs)
 ),
 rn_enrollments as
---with rn_enrollments as
 (
   select *,
     row_number() over(partition by year_month, code_rncp, code_rs, certification_title, code_certification, provider_id, provider, code_formacode_1, code_formacode_2, code_formacode_3, code_formacode_4, code_formacode_5) as rn
